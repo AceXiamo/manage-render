a9c242dc5fccf91f4ad7bd0fddd10cfe59929593
AceXiamo <xiamo@qwq.link>
2024-02-01 11:05:28 +0800

    Add virtual:uno.css import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import 'virtual:uno.css'
+
 export { default as DataForm } from './core/DataForm'
 export { default as DataTable } from './core/DataTable'
 export { default as QueryForm } from './core/QueryForm'
 export * from './core/FormFields'
 
-import 'virtual:uno.css'
