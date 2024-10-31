4585b6baf8f5f2fe2077a379cba8077b74321bbf
AceXiamo <xiamo@qwq.link>
2024-10-31 09:51:52 +0800

    feat: dialog component support fullscreen

--- src/components/Dialog/dialog.vue
+++ src/components/Dialog/dialog.vue
@@ -1,52 +1,62 @@
 <template>
+  <div
+    v-if="show"
+    v-motion="$props.motionKey!"
+    class="bg-black bg-opacity-20 fixed inset-0 z-[999] overflow-auto"
+    :initial="{
+      opacity: 0,
+      y: 10,
+      transition: {
+        duration: 200
+      }
+    }"
+    :enter="{
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: 'easeOut'
+      }
+    }"
+    :leave="{
+      opacity: 0,
+      y: 10,
+      transition: {
+        ease: 'easeOut'
+      }
+    }"
+    @click="leave"
+  >
     <div
-        v-if="show"
-        v-motion="$props.motionKey!"
-        class="bg-black bg-opacity-20 fixed inset-0 z-[999] overflow-auto"
-        :initial="{
-            opacity: 0,
-            y: 10,
-            transition: {
-                duration: 200,
-            },
-        }"
-        :enter="{
-            opacity: 1,
-            y: 0,
-            transition: {
-                ease: 'easeOut',
-            },
-        }"
-        :leave="{
-            opacity: 0,
-            y: 10,
-            transition: {
-                ease: 'easeOut',
-            },
-        }"
-        @click="leave"
+      class="bg-white dark:bg-[#141414] min-h-[100px] h-max flex flex-col rounded-md py-[20px] gap-[20px] absolute inset-0"
+      :style="{
+        width: fullScreen
+          ? '100vw'
+          : typeof $props.width === 'number'
+            ? `${$props.width}px`
+            : $props.width,
+        height: fullScreen ? '100vh' : 'auto',
+        margin: fullScreen ? '0' : '100px auto'
+      }"
+      @click.stop
     >
-        <div
-            class="bg-white dark:bg-[#141414] min-h-[100px] h-max flex flex-col rounded-md py-[20px] gap-[20px] absolute inset-0 m-[100px_auto]"
-            :style="{ width: typeof $props.width === 'number' ? `${$props.width}px` : $props.width }"
-            @click.stop
-        >
-            <div class="flex items-center px-[20px]">
-                <span class="text-[#333] dark:text-white font-bold text-[15px] select-none">{{ $props.title }}</span>
-                <div class="ml-auto cursor-pointer" @click="leave">
-                    <el-icon :size="24" color="#E74745">
-                        <CircleCloseFilled />
-                    </el-icon>
-                </div>
-            </div>
-            <template v-if="$props.content">
-                <component :is="$props.content(h)" />
-            </template>
-            <template v-if="$props.footer">
-                <component :is="$props.footer(h)" />
-            </template>
+      <div class="flex items-center px-[20px]">
+        <span class="text-[#333] dark:text-white font-bold text-[15px] select-none">{{
+          $props.title
+        }}</span>
+        <div class="ml-auto cursor-pointer" @click="leave">
+          <el-icon :size="24" color="#E74745">
+            <CircleCloseFilled />
+          </el-icon>
         </div>
+      </div>
+      <template v-if="$props.content">
+        <component :is="$props.content(h)" />
+      </template>
+      <template v-if="$props.footer">
+        <component :is="$props.footer(h)" />
+      </template>
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -55,33 +65,39 @@ import { onMounted, ref, h, VNode } from 'vue'
 import { CircleCloseFilled } from '@element-plus/icons-vue'
 
 export type DialogProps = {
-    title: string
-    content?: (h: any) => VNode
-    footer?: (h: any) => VNode
-    width?: string | number
-    appendToBody?: boolean
-    onClose?: (key: string) => void
-    motionKey?: string
+  title: string
+  content?: (h: any) => VNode
+  footer?: (h: any) => VNode
+  width?: string | number
+  appendToBody?: boolean
+  onClose?: (key: string) => void
+  motionKey?: string
 }
 
 const show = ref(false)
 const motion = useMotions()
 const props = withDefaults(defineProps<DialogProps>(), {
-    title: '',
+  title: ''
 })
 
 onMounted(() => {
-    show.value = true
+  show.value = true
 })
 
 const leave = () => {
-    motion[props.motionKey!].leave(() => {
-        show.value = false
-        props.onClose?.(props.motionKey!)
-    })
+  motion[props.motionKey!].leave(() => {
+    show.value = false
+    props.onClose?.(props.motionKey!)
+  })
+}
+
+const fullScreen = ref(false)
+const toggleFullScreen = () => {
+  fullScreen.value = !fullScreen.value
 }
 
 defineExpose({
-    leave,
+  leave,
+  toggleFullScreen
 })
 </script>
